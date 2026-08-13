8c8ac02a219e915a2218296618cbc83125a92d56
Aryanbuha890 <aryan.buha89@gmail.com>
2026-08-13 12:39:47 +0530

    chore: resolve merge conflicts with main

--- backend/controllers/readinessRoutes.js
+++
@@ -1,3 +0,0 @@
-// DEPRECATED: This file was created in controllers/ by error in upstream merge.
-// The active routes are located in backend/routes/readinessRoutes.js instead.
-module.exports = require('../routes/readinessRoutes');
