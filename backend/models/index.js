86c6c5eb8317cdf1e3a3bbd76ee4c30c04d8052f
Aryanbuha890 <aryan.buha89@gmail.com>
2026-08-24 22:29:59 +0530

    feat: implement daily revision digest dispatcher with email, telegram bot and webhooks #1652

--- backend/models/index.js
+++ backend/models/index.js
@@ -46,6 +46,7 @@ const QuizRoom = require('./QuizRoom');
 const AuditLog = require('./AuditLog');
 const HandwrittenSubmission = require('./HandwrittenSubmission');
 const LearningPath = require('./LearningPath');
+const NotificationSettings = require('./NotificationSettings');
 
 // User associations
 User.hasMany(Exam, { foreignKey: 'user', onDelete: 'CASCADE' });
@@ -70,6 +71,7 @@ User.hasMany(ActivityLog, { foreignKey: 'user', onDelete: 'CASCADE' });
 User.hasMany(Achievement, { foreignKey: 'userId', as: 'achievements', onDelete: 'CASCADE' });
 User.hasMany(UserBadge, { foreignKey: 'userId', as: 'badgesRef', onDelete: 'CASCADE' });
 User.hasMany(Folder, { foreignKey: 'userId', onDelete: 'CASCADE' });
+User.hasOne(NotificationSettings, { foreignKey: 'userId', as: 'notificationSettings', onDelete: 'CASCADE' });
 
 // Exam associations
 Exam.belongsTo(User, { foreignKey: 'user', as: 'userRef' });
@@ -304,4 +306,5 @@ module.exports = {  sequelize,  User,  Exam,
   QuizRoom,
   AuditLog,
   HandwrittenSubmission,
+  NotificationSettings,
 };
