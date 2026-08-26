fa8287907193162e06e2512312f08906dd456464
Aryanbuha890 <aryan.buha89@gmail.com>
2026-08-26 17:15:16 +0530

    feat: implement Operational Transformation collaborative note sync engine #1797

--- backend/server.js
+++ backend/server.js
@@ -533,6 +533,7 @@ require('./sockets/focusRoomHandler')(io);
 require('./sockets/studyRoomSocket')(io);
 require('./sockets/interviewSocket')(io);
 require('./sockets/interviewSignalling')(io);
+require('./sockets/noteSyncHandler')(io);
 require('./services/webrtcSignalingService')(io);
 // Authenticate Socket.io connections
 io.use((socket, next) => {
@@ -562,6 +563,9 @@ io.on('connection', (socket) => {
 // Start background schedulers
 const { startScheduler } = require('./services/weeklyDigestService');
 startScheduler();
+
+const { startReconciliationScheduler } = require('./services/otSyncService');
+startReconciliationScheduler();
 
 const { initStudyReminderCron } = require('./jobs/studyReminderCron');
 const { initStreakReminderCron } = require('./jobs/streakReminderCron');
@@ -603,6 +607,14 @@ const gracefulShutdown = (signal) => {
   server.close(async () => {
     logger.info('HTTP connections drained, closing resource pools');
     clearTimeout(forceExitTimeout);
+
+    try {
+      const { stopReconciliationScheduler } = require('./services/otSyncService');
+      stopReconciliationScheduler();
+      logger.info('OT reconciliation scheduler stopped');
+    } catch (otErr) {
+      logger.error('error stopping OT reconciliation scheduler', { err: otErr });
+    }
 
     try {
       const { stopWorker } = require('./workers/squadActivityWorker');
